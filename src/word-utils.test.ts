590bd5da99c9a907e67875d6582cf681277e766b
vickyruud <aprajit.k@gmail.com>
2022-03-11 14:25:26 -0500

    add word bank and test for generate random word

---
+++ src/word-utils.test.ts
@@ -0,0 +1,10 @@
+
+import { getRandomWord } from './word-utils'
+import { render, screen } from './test/test-utils'
+
+describe('Word Utils', () => {
+  it('generates random word', () => {
+    expect(getRandomWord()).toEqual('sugar');
+  })
+
+})
